781759ef4fb88a91901f1e72646416b6811e53f3
Harry27PL <jakub.zawislak@limtel.pl>
2015-11-15 20:52:24 +0100

    przeróbka na unipolarną

--- 6.6_propagacja_wsteczna_beta/NeuronSigmoidal.php
+++ 6.6_propagacja_wsteczna_beta/NeuronSigmoidal.php
@@ -7,13 +7,20 @@ class NeuronSigmoidal extends Neuron
         $this->data = $data;
 
         $result = tanh($this->sum() * $this->beta * $this->betaWeight);
+//        $result = tanh($this->sum() * $this->beta * $this->betaWeight);
 
         return $result;
     }
 
+    private function activate($x)
+    {
+        return 1 / (1 + pow(exp(1), -$this->beta * $x));
+    }
+
     public function getDerrivateOfLastSum()
     {
-        return $this->beta * $this->betaWeight * (1 - pow(tanh($this->getLastSum()), 2));
+        return $this->beta * $this->activate($this->getLastSum()) * (1 - $this->activate($this->getLastSum()));
+//        return $this->beta * $this->betaWeight * (1 - pow(tanh($this->getLastSum()), 2));
     }
 
 }
